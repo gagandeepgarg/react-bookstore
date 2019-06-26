a08e0da77febd28ef5a9de191b7a939e541c8cc2
ggarg-c <ggarg-c@alierahealthcare.com>
2019-06-26 11:15:34 +0530

    global error added

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Form, Button} from 'semantic-ui-react';
-import InlineError from '../messages/InlineError';
 import  PropTypes from 'prop-types';
+import {Form, Button, Message} from 'semantic-ui-react';
+import InlineError from '../messages/InlineError';
 
 class LoginForm extends React.Component{
     state={
@@ -19,7 +19,9 @@ class LoginForm extends React.Component{
         const errors = this.validate(this.state.data);
         this.setState({errors});
         if(Object.keys(errors).length===0){
-            this.props.submit(this.state.data);
+            this.setState({loading:true});
+            this.props.submit(this.state.data)
+            .catch(err => this.setState({errors:err.response.data.errors, loading:false}))
         }
     }
     validate =(data)=>{
@@ -29,10 +31,14 @@ class LoginForm extends React.Component{
         return errors;
     }
     render() {
-        const {data, errors} = this.state;
+        const {data, errors, loading} = this.state;
 
         return (
-            <Form onSubmit={this.onSubmit}>
+            <Form onSubmit={this.onSubmit} loading={loading}>
+                {errors.global && <Message negative>
+                    <Message.Header>Something went wrong</Message.Header>
+                    <p>{errors.global}</p>
+                    </Message>}
                 <Form.Field error={!!errors.username}>
                     <label htmlFor="username">Username</label>
                     <input type="text" id="username" name="username" placeholder="Enter Username"
