fafbb8e3cd2b5dad74c823f95f1fee122d53f0c2
ggarg-c <ggarg-c@alierahealthcare.com>
2019-07-31 22:25:43 +0530

    loaded order details in popup

--- src/components/pages/OrdersPage.js
+++ src/components/pages/OrdersPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
-import { Message, Icon, Grid,Button } from 'semantic-ui-react';
+import { Message, Icon, Grid,Button, Header, Modal } from 'semantic-ui-react';
 import {loadUserOrdersData} from '../../actions/order';
 import HeaderPage from '../pages/HeaderPage';
 
@@ -11,7 +11,9 @@ class OrdersPage extends React.Component{
         success: false,
         pageNumber :1,
         filesPerPage : 10,
-        userOrders:[]
+        userOrders:[],
+        modalOpen:false,
+        selectedOrder:{}
     }
     componentDidMount() {
         this.props.loadUserOrdersData(this.state.pageNumber, this.state.filesPerPage,
@@ -20,11 +22,15 @@ class OrdersPage extends React.Component{
             .catch(() => this.setState({ loading: false, success: false }));
     }
     orderDetails = (order)=>{
-        console.log(order);
+        this.setState({modalOpen:true,selectedOrder:order});
+    }
+    handleClose =()=>{
+        console.log('called');
+        this.setState({modalOpen:false, selectedOrder:{}});
     }
     
     render() {
-        const { loading, success, userOrders } = this.state;
+        const { loading, success, userOrders, modalOpen, selectedOrder } = this.state;
         return(
             <div>
             <HeaderPage/>
@@ -71,7 +77,6 @@ class OrdersPage extends React.Component{
                                 <Grid.Column>
                                     <Button primary onClick={()=>this.orderDetails(order)}>View Order Details</Button> 
                                 </Grid.Column>
-                                
                             </Grid.Row>
                         )}
                         </Grid>
@@ -88,6 +93,61 @@ class OrdersPage extends React.Component{
                     </Message.Content>
                 </Message>
             )}
+            <Modal
+                open={modalOpen}
+                onClose={this.handleClose}
+                basic
+                size='small'>
+                <Header > 
+                    <div className="width-100">OrderId: {selectedOrder._id}  
+                        <span className="right-float">
+                            Date:{new Date(selectedOrder.createdAt).toLocaleString()}
+                        </span>
+                    </div>
+                    Total Order Price:  {selectedOrder.TotalOrderPrice}
+                </Header>
+                <Modal.Content>
+                {selectedOrder.orderItems && <div className="container">
+                            <Grid columns={5} divided>
+                                <Grid.Row>
+                                    <Grid.Column>
+                                        Bookname    
+                                    </Grid.Column>
+                                    <Grid.Column>
+                                        Price PerItem
+                                    </Grid.Column>
+                                    <Grid.Column>
+                                        Qunatity
+                                    </Grid.Column>
+                                    <Grid.Column>
+                                        Total Price
+                                    </Grid.Column>
+                                </Grid.Row>
+                            {selectedOrder.orderItems.map((cartItem) =>
+                                <Grid.Row>
+                                    <Grid.Column>
+                                        {cartItem.bookName}    
+                                    </Grid.Column>
+                                    <Grid.Column>
+                                        {cartItem.pricePerUnit}
+                                    </Grid.Column>
+                                    <Grid.Column>
+                                        {cartItem.quantity}
+                                    </Grid.Column>
+                                    <Grid.Column>
+                                        {cartItem.pricePerUnit * cartItem.quantity}
+                                    </Grid.Column>
+                                </Grid.Row>
+                            )}
+                            </Grid>
+                        </div>}
+                </Modal.Content>
+                <Modal.Actions>
+                <Button color='green' onClick={this.handleClose} inverted>
+                    <Icon name='checkmark' /> Got it
+                </Button>
+                </Modal.Actions>
+            </Modal>
         </div>
         )
     }
